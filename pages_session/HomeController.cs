1f6a03c6210ae9ac9ba964b1c2796a9de502fe1b
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2020-04-06 21:29:31 +0300

    Add files via upload

---
+++ pages_session/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb0604.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        // localhost:9006/home/index
+        public ActionResult Index()
+        {
+
+            //return Content("Hello world!");
+
+            //return Redirect("https://www.ynet.co.il/home/0,7340,L-8,00.html");
+
+            return new FilePathResult("~/Views/Home/loging.html", "text/html");
+        }
+
+        // localhost:9006/home/data
+        public ActionResult data()
+        {
+
+            //return Content("Hello world!");
+
+            //return Redirect("https://www.ynet.co.il/home/0,7340,L-8,00.html");
+
+            return new FilePathResult("~/Views/Home/data.html", "text/html");
+        }
+
+        public ActionResult DataProcess()
+        {
+            string item = Request.Params["itemtxt"];
+            //Session["item"] = item;
+            HttpContext.Application["item"] = item;
+
+            return Content($"{item} recieved!");
+        }
+
+        public ActionResult Whatsmydata()
+        {
+
+            return Content($"data =  {HttpContext.Application["item"]}");
+        }
+
+        public ActionResult LoginProcess()
+        {
+            string name = Request.Params["nametxt"];
+            Session["username"] = name;
+
+            return Content($"Hello {name}");
+        }
+
+        public ActionResult Whatsmyname()
+        {
+
+            return Content($"Hello {Session["username"]}");
+        }
+    }
+}
---
+++ pages_session/PageController.cs
@@ -0,0 +1,40 @@
+﻿using MyWeb0604.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb0604.Controllers
+{
+    public class PageController : Controller
+    {
+        // localhost:9006/page/hello
+        public ActionResult Hello()
+        {
+            return new FilePathResult("~/Views/Page/hello.html", "text/html");
+        }
+
+        IList<Student> studentList = new List<Student>() {
+                    new Student(){ StudentID=1, StudentName="Steve", Age = 21 },
+                    new Student(){ StudentID=2, StudentName="Bill", Age = 25 },
+                    new Student(){ StudentID=3, StudentName="Ram", Age = 20 },
+                    new Student(){ StudentID=4, StudentName="Ron", Age = 31 },
+                    new Student(){ StudentID=5, StudentName="Rob", Age = 19 }
+                };
+
+        // localhost:9006/page/razordemo
+        public ActionResult razordemo()
+        {
+            //ViewData["students"] = studentList;
+
+            ViewBag.students = studentList; // dynamic property
+
+            //return View("~/Views/Home/ViewPage1.cshtml");
+
+            //return View(); // razordemo.cshtml
+
+            return View("~/Views/Page/demo.cshtml");
+        }
+    }
+}
